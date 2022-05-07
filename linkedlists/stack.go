b01748c88ddab1b4fc0a297060284e0c1fd7ba4c
keith <keithontiveros@ucla.edu>
2022-05-06 19:36:23 -0700

    Restructure files

---
+++ linkedlists/stack.go
@@ -0,0 +1,20 @@
+package linkedlists
+
+type Stack struct {
+	first *node
+	N     int
+}
+
+func (s *Stack) Push(item int) {
+	oldFirst := s.first
+	s.first = &node{item, oldFirst}
+	s.N++
+}
+
+func (s *Stack) Pop() int {
+	item := s.first.item
+	s.first = s.first.next
+	s.N--
+
+	return item
+}
